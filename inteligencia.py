72826b58e74e7ac26b5bf0f5e20513828435e8ad
DE LA HOZ SANJUAN DANIEL <ddelahoz28@cuc.edu.co>
2022-03-02 19:42:34 -0500

    primer punto

--- inteligencia.py
+++ inteligencia.py
@@ -5,4 +5,10 @@ Spyder Editor
 This is a temporary script file.
 """
 # variable 
-a = 0
+donacion = int(input("ingrese la donacion"))
+telecomunicaciones = .1 * donacion
+sistemas = .25* donacion
+administracion = .35* donacion
+contabilidad = .30*donacion
+
+print( donacion, telecomunicaciones, sistemas, administracion, contabilidad)

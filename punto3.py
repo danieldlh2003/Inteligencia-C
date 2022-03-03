18f96fad175fa8d5ce1bb0b4554b5fca49301414
DE LA HOZ SANJUAN DANIEL <ddelahoz28@cuc.edu.co>
2022-03-02 20:22:48 -0500

    punto3

---
+++ punto3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 20:09:01 2022
+
+@author: ddelahoz28
+"""
+
+#punto3
+sueldo = int(input("el sueldo es:" ))
+comision = sueldo * 0.15
+sueldototal = sueldo + comision
+print( f"el sueldo base es:{sueldo} la comision es:{comision} sueldo total es: {sueldototal}")
+
